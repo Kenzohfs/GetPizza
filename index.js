609618844fc6f44ad108e08ffbd49d80e1d508c9
Kenzohfs <kenzo_sato@estudante.sc.senai.br>
2022-10-04 08:39:57 -0300

    04/10 - 08:39
    Roteamento feito

---
+++ index.js
@@ -0,0 +1,22 @@
+function avancarForms(etapaNum) {
+    switch (etapaNum) {
+        case 1:
+            window.location.href = "dadosPessoais.html"
+            break;
+        case 2:
+            window.location.href = "tamanho.html"
+            break;
+        case 3:
+            window.location.href = "sabores.html"
+            break;
+        case 4:
+            window.location.href = "adicionais.html"
+            break;
+        case 5:
+            window.location.href = "entrega.html"
+            break;
+        case 6:
+            window.location.href = "confirma.html"
+            break;
+    }
+}
