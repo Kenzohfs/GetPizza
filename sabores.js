8c59bed16dece40d24a84745489887b959778f63
Kenzohfs <kenzo_sato@estudante.sc.senai.br>
2022-10-04 19:19:24 -0300

    04/10 - 19:19
    feito counter de sabores

--- sabores.js
+++ sabores.js
@@ -2,9 +2,15 @@ let saboresAtuais = 0;
 
 function formatarNumeroSabores() {
     let elemento = document.getElementById("numeroSabores");
+    saboresAtuais = document.querySelectorAll(".selected-sabor").length;
+    console.log(saboresAtuais);
     let numeroSabores = JSON.parse(localStorage.getItem("PEDIDO")).tamanho.qtdsabores;
 
     elemento.innerText = saboresAtuais + " / " + numeroSabores;
+
+    setTimeout(() => {
+        formatarNumeroSabores();
+    }, 1000)
 }
 
 formatarNumeroSabores();
