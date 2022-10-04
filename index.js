3b8c75a2f6a39a6e65c2259544c9e81d04d8686f
Kenzohfs <kenzo_sato@estudante.sc.senai.br>
2022-10-04 09:52:44 -0300

    04/10 - 09:52
    feito um pouco do js

--- index.js
+++ index.js
@@ -1,22 +1,84 @@
 function avancarForms(etapaNum) {
+    let informacoes;
     switch (etapaNum) {
         case 1:
-            window.location.href = "dadosPessoais.html"
+            window.location.href = "dadosPessoais.html";
             break;
         case 2:
-            window.location.href = "tamanho.html"
+            informacoes = { pedido: buscarDadosPessoais() };
+            window.location.href = "tamanho.html";
             break;
         case 3:
-            window.location.href = "sabores.html"
+            informacoes = { tamanho: buscarDadosTamanho() };
+            window.location.href = "sabores.html";
             break;
         case 4:
-            window.location.href = "adicionais.html"
+            informacoes = { sabores: buscarDadosSabores() };
+            window.location.href = "adicionais.html";
             break;
         case 5:
-            window.location.href = "entrega.html"
+            informacoes = { adicionais: buscarDadosAdicionais() };
+            informacoes = { bebidas: buscarDadosBebidas() };
+            window.location.href = "entrega.html";
             break;
         case 6:
-            window.location.href = "confirma.html"
+            informacoes = { entrega: buscarDadosEntrega() };
+            window.location.href = "confirma.html";
+            break;
+        case 7:
+            informacoes = { confirma: buscarDadosConfirma() };
+            window.location.href = "confirma.html";
             break;
     }
+
+    localStorage.setItem("PEDIDO", JSON.stringify(informacoes));
+}
+
+function buscarDadosPessoais() {
+    let nome = document.getElementById("nomePessoa").value;
+    let cpf = document.getElementById("cpfPessoa").value;
+    let email = document.getElementById("emailPessoa").value;
+    let telefone = document.getElementById("telefonePessoa").value;
+
+    return { nome, cpf, email, telefone };
+}
+
+function buscarDadosTamanho() {
+    let tamanhoElement = document.querySelector(".selected-tamanho");
+    
+    return JSON.parse(tamanhoElement.ariaValueText);
+}
+
+function buscarDadosSabores() {
+    let saboresElements = document.querySelectorAll(".sabores");
+
+    let sabores = [];
+    for (let saborElement of saboresElements) {
+        sabores.push({ sabor: saborElement.value, preco: saborElement.preco });
+    }
+
+    return sabores;
+}
+
+function buscarDadosAdicionais() {
+    let adicional = document.getElementById("adicionalAdicional").value;
+    let preco = document.getElementById("precoAdicional").value;
+
+    return { adicional, preco };
+}
+
+function buscarDadosBebidas() {
+    let bebida = document.getElementById("adicionalAdicional").value;
+    let preco = document.getElementById("precoAdicional").value;
+
+    return { adicional, preco };
+}
+
+function selecionarTamanho(idTamanho) {
+    let tamanhoElements = document.querySelectorAll(".tamanho");
+    for (let tamanhoElement of tamanhoElements) {
+        tamanhoElement.className = "tamanho";
+    }
+    let tamanho = document.getElementById(idTamanho);
+    tamanho.classList.add("selected-tamanho");
 }
